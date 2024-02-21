9259b7c5a8104c7a485e50463d4fa8c761d9b405
Harsh kumar <hk342064@gmail.com>
2024-02-21 10:09:52 +0530

    K closest Elements

---
+++ Heap/Find K-Closest-Elements.cpp
@@ -0,0 +1,18 @@
+#define pii pair<int, int>
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int i=0,j=arr.size()-1;
+        
+        while(j-i+1>k){
+            if(abs(arr[i]-x) > abs(arr[j]-x)){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        vector<int> ans(arr.begin()+i,arr.begin()+j+1);
+        return ans;
+    }
+};
