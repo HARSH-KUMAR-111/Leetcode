3cb965a6e792fb8361219a384cd002e68e062adb
Harsh Kumar <92089591+HARSH-KUMAR-111@users.noreply.github.com>
2024-07-13 15:01:26 +0530

    Time: 217 ms (61.83%), Space: 223.1 MB (51.14%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,68 +1,57 @@
+// TC->O(nlogn+ 3*n)
+// SC->O(2*2*n) = O(n)
+
 class Solution {
 public:
+    int IndexVal(vector<int> &pos,int val){
+        for(int i=0;i<pos.size();i++){
+            if(val==pos[i]) return i;
+        }
+        return -1;
+    }
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
         int n=positions.size();
-        map<int , pair<int , char>> mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[positions[i]].first=healths[i];
-            mp[positions[i]].second=directions[i];
-        }
-        stack<pair<int , pair<int , char>>> st;
-        for(auto it:mp)
-        {
-            if(!st.empty() && it.second.second=='L')
-            {
+        stack<pair<pair<int,int>,pair<char,int>>> st;
+        vector<pair<pair<int,int>,pair<char,int>>> vc;
 
-                if(st.top().second.second=='R')
-                {
-                    if(it.second.first<st.top().second.first)
-                    {
-                        st.top().second.first-=1;
-                        continue;
-                    }
-                    else if(st.top().second.first<it.second.first)
-                    {
-                        while(!st.empty() && it.second.first>st.top().second.first &&st.top().second.second=='R' )
-                        {
-                            it.second.first-=1;
-                            st.pop();
-                        }
-                        if(!st.empty() && it.second.first==st.top().second.first &&  st.top().second.second=='R')
-                        {
-                            st.pop();
-                            continue;
-                        }
-                        if(!st.empty() && it.second.first<st.top().second.first &&  st.top().second.second=='R' )
-                        {
-                            st.top().second.first-=1;
-                            continue;
-                        }
-   
-                    }
-                    else
-                    {
-                        st.pop();
-                        continue;
-                    }
-                }   
-            }
-            st.push(it);
+        for(int i=0;i<n;i++){
+            vc.push_back({{positions[i],i},{directions[i],healths[i]}});
         }
-        vector<int> res;
-        unordered_map<int , int> mp1;
-        while(!st.empty())
-        {
-           mp1[st.top().first]=st.top().second.first;
+        sort(vc.begin(),vc.end());
+        // for(auto it:vc){
+        //     cout<<it.first.first<<" "<<it.first.second<<" "<<it.second.first<<" "<<it.second.second<<endl;
+        // }
+
+        for(int i=0;i<n;i++){
+            int x =vc[i].second.first;
+            int y = vc[i].second.second;
+            while( x=='L' && !st.empty() && st.top().second.first =='R' && y >  st.top().second.second)
+            {
+                y-=1;
+                st.pop();
+            }
+            if(x=='L' && !st.empty() && st.top().second.first =='R' && y <  st.top().second.second){
+                st.top().second.second -= 1;
+                continue;
+            }
+            else if (x=='L' && !st.empty() && st.top().second.first =='R' && y ==  st.top().second.second){
+                st.pop();
+                continue;
+            }
+            st.push({{vc[i].first.first,vc[i].first.second},{vc[i].second.first, y}});
+        }
+
+        vector<int> ans(n,-1);
+        int size = st.size();
+        while(!st.empty()){
+            int idx = st.top().first.second;
+            ans[idx]=st.top().second.second;
             st.pop();
         }
-        for(int i=0;i<n;i++)
-        {
-            if(mp1.find(positions[i])!=mp1.end())
-            {
-                res.push_back(mp1[positions[i]]);
-            }
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            if(ans[i] !=-1) res.push_back(ans[i]);
         }
-        return res;    
+        return res;
     }
 };
