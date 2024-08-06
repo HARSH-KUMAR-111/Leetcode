d4cf76b164937def942515724fc46398b849637a
Harsh Kumar <92089591+HARSH-KUMAR-111@users.noreply.github.com>
2024-08-06 11:20:40 +0530

    Time: 42 ms (55.34%), Space: 45.1 MB (24.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,20 @@
+// TC->O(2*k)
+// SC->O(1)
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
+        int leftS = 0;
+        for(int i=0;i<k;i++) leftS+= cardPoints[i];
 
-        if(n == k) return accumulate(cardPoints.begin(),cardPoints.end(),0);
-        
-        vector<int> pref(n,0);
-        pref[0] = cardPoints[0];
-        for(int i=1;i<n;i++){
-            pref[i] = pref[i-1]+cardPoints[i];
+        int rightS = 0;
+        int j = n-1,maxi = leftS;
+        for(int i =k-1;i>=0;i--){
+            leftS-=cardPoints[i];
+            rightS+=cardPoints[j--];
+            maxi = max(leftS+rightS,maxi);
+            
         }
-
-        vector<int> suff(n,0);
-        suff[n-1] = cardPoints[n-1]; 
-        for(int i = n-2;i>=0;i--){
-            suff[i] = suff[i+1]+cardPoints[i];
-        }
-
-        int j =k-2,i= n-1;
-        int maxSum = max(suff[n-k],pref[k-1]);
-        while(j>=0){
-            maxSum = max(maxSum,pref[j--]+suff[i--]);
-        }
-        return maxSum;
+        return maxi;
     }
 };
